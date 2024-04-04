735dbfb15ed9b62708f772e3591fe0234d312894
Anatoliy-Usov <sigal.kmua@gmail.com>
2024-04-04 21:35:09 +0300

    layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+// import { Container } from './Fitback/fitback.style';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <div>
+          <button>Goog</button>
+          <button>Nautral</button>
+          <button>Bad</button>
+        </div>
+
+        <h1>Statistics</h1>
+        <p>Goog: {this.state.good}</p>
+        <p>Nautral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+}
